3cac925dabbf04ddae40b17ba5d90d5c5c9faae8
Bnesngo <Bnesngo@users.noreply.github.com>
2015-12-05 10:48:29 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from time import time.sleep as wait
+
+import sys
+
+w = 250 #To store
+h = 250 #Also to store
+
+tk = Tk()
+wn = Canvas(width=w, height=250)
+
+tk.title("A Random Game")
